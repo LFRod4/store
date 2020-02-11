e9f8542bd612de6968bc7b36572dd1f9b5536c25
Luis Rodriguez <baesicmedia@gmail.com>
2020-02-10 19:37:15 -0800

    Update wsgi.py

--- ecomstore/wsgi.py
+++ ecomstore/wsgi.py
@@ -1,8 +1,8 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecomstore.settings")
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
